a04c86d83aedef816b1ac2766a6b8bb6c24a9485
Clendenin <Clendenin@protonmail.com>
2018-01-27 12:33:00 -0600

    Beginning of Daemon app

---
+++ Redtooth/Redtooth.py
@@ -0,0 +1,21 @@
+//pip install python-daemon
+import os
+import time
+from daemon import runner
+
+class Redtooth():
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = 'dev/tty/'
+        self.pidfile_path = '/tmp/foo.pid'
+        self.pidfile_timeout = 5
+    def run(self):
+        while True:
+            print("Flag")
+            os.system("rfkill block bluetooth")
+            time.sleep(10)
+
+app = Redtooth()
+daemon_runner = runner.DaemonRunner(app)
+daemon_runner.do_action()
