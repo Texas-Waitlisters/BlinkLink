9ce8857da9b3aecc288d3235fb6d5f531521b691
Keven Li <kevenli@utexas.edu>
2018-01-27 14:53:19 -0600

    remove bluetooth

--- Redtooth/Redtooth.py
+++ Redtooth/Redtooth.py
@@ -1,9 +1,8 @@
 #pip install python-daemon
-#test
 import os
 import time
-import bluetooth
 from daemon import runner
+
 
 class Redtooth():
     def __init__(self):
@@ -12,11 +11,13 @@ class Redtooth():
         self.stderr_path = 'dev/tty/'
         self.pidfile_path = '/tmp/foo.pid'
         self.pidfile_timeout = 5
+
     def run(self):
         while True:
             print("Flag")
             os.system("rfkill block bluetooth")
             time.sleep(10)
+
 
 app = Redtooth()
 daemon_runner = runner.DaemonRunner(app)
