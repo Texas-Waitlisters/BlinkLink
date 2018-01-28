8f3c9e06c547a5c721d833ef90488ef6e49cd069
Keven Li <kevenli@utexas.edu>
2018-01-28 00:54:01 -0600

    Audio state
    Audio state output

--- Redtooth/RedtoothWindows/AudioLevelWrapper.py
+++ Redtooth/RedtoothWindows/AudioLevelWrapper.py
@@ -1,12 +1,9 @@
-import ctypes
+import subprocess
+import time
 
+audiovalue = subprocess.Popen("ConsoleApplication1.exe", shell=False, stdout=subprocess.PIPE)
 
-def checkaudio():
-    audiovaluedll = ctypes.CDLL('Redtooth\RedtoothWindows\CScore\CSCore.dll')
-
-    print(audiovalue)
-    #return audiovalue
-
-
-print("hi")
-
+val = audiovalue.communicate()[0].strip() == b'True'
+print(val)
+time.sleep(.15)
+audiovalue.kill()
