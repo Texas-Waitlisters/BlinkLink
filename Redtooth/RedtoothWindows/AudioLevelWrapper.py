a2c1dd76dd950939ed1a504c3d23ba3a83a3ddc2
Keven Li <kevenli@utexas.edu>
2018-01-28 02:27:44 -0600

    CSCore consolidation

--- Redtooth/RedtoothWindows/AudioLevelWrapper.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import time
-
-audiovalue = subprocess.Popen("ConsoleApplication1.exe", shell=False, stdout=subprocess.PIPE)
-
-val = audiovalue.communicate()[0].strip() == b'True'
-print(val)
-time.sleep(.15)
-audiovalue.kill()
